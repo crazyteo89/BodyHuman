d5c4187f21b7ef9af89d93798df7f52d42735bc3
Alessandro Armando <alessandroarmando@par-57.local>
2017-04-03 19:30:30 +0200

    Test passaggio dati per la split view

---
+++ Body/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  Body
+//
+//  Created by Alessandro Armando on 03/04/17.
+//  Copyright © 2017 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
